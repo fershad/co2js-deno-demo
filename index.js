54eb88ba106e3963fb17e2565d073e41be307bc4
fershad <27988517+fershad@users.noreply.github.com>
2022-12-15 20:42:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ const bytesSent = 1000 * 1000 * 1000; // 1GB expressed in bytes
 const estimatedCO2 = emissions.perByte(bytesSent);
 console.log(`Sending a gigabyte, had a carbon footprint of ${estimatedCO2.toFixed(3)} grams of CO2.`);
 
-const greenHost = hosting.check('google.com')
+const greenHost = await hosting.check('google.com')
 console.log(`Google.com ${greenHost ? 'is' : 'is not' } on green hosting.`);
+
+// Run the command `deno run --allow-read --allow-env --allow-net index.js` in the terminal
